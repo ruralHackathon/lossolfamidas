a745331fa9953c77de5f6e2b0e5b015966ebcede
Alvaro Mendez Alves <alvaromendezalves@gmail.com>
2018-12-28 20:02:19 +0100

    Mapa medianamente hecho

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,39 @@
+// webpack.config.js
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // directory where compiled assets will be stored
+    // the project directory where all compiled assets will be stored
     .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
+
+    // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
-     */
+    // will create public/build/app.js and public/build/app.css
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    // .addEntry('midatatable', './assets/js/midatatable.js')
+    .addStyleEntry('appstyle', './assets/css/app.css')
+   
 
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
 
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    // enable source maps during development
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
 
-    // enables Sass/SCSS support
-    //.enableSassLoader()
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    // create hashed filenames (e.g. app.abc123.css)
+    // .enableVersioning()
 
-    // uncomment if you use API Platform Admin (composer req api-admin)
-    //.enableReactPreset()
-    //.addEntry('admin', './assets/js/admin.js')
+    // allow sass/scss files to be processed
+    // .enableSassLoader()
 ;
 
+// export the final configuration
 module.exports = Encore.getWebpackConfig();
